1d4bd6fa22f5381cdf6716394e7169e1004fa750
Mintellity GmbH <info@mintellity.com>
2024-02-19 11:43:35 +0100

    Update DocumentController.php

--- src/Http/Controllers/DocumentController.php
+++ src/Http/Controllers/DocumentController.php
@@ -36,7 +36,7 @@ class DocumentController extends Controller
         $filePath = DocumentService::getFilePath($document);
 
         if (Storage::exists($filePath)) {
-            return Storage::download($filePath, $document->name);
+            return Storage::download($filePath, $document->document_name);
         }
 
         return redirect()->back()->with('error', 'Datei nicht gefunden.');
